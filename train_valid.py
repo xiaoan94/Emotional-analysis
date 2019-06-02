a880efc83520f91366e2eea42792df1525b191c1
sharns96 <41625775+sharns96@users.noreply.github.com>
2019-06-02 21:23:06 +0800

    Add files via upload

--- train_valid.py
+++ train_valid.py
@@ -2,154 +2,223 @@ import gensim
 import numpy as np
 import pandas as pd
 import jieba
-
+import torch
+from sklearn.metrics import f1_score
 
 class PreProcessor(object):
     def __init__(self, filename, busi_name="location_traffic_convenience"):
         self.filename = filename
         self.busi_name = busi_name
-        self.embedding_dim = 150
+        # self.embedding_dim = 150
 
-        # 读取词向量
-        # embedding_file = "./word_embedding/word2vec_wx"
         self.word2vec_model = gensim.models.Word2Vec.load('word2vec_150_3')
 
     # 读取原始csv文件
     def read_csv_file(self):
-        # reload(sys)
-        # sys.setdefaultencoding('utf-8')
-        # print("after coding: " + str(sys.getdefaultencoding()))
 
         data = pd.read_csv(self.filename, sep=',')
         x = data.content.values
         y = data[self.busi_name].values
+        y = list(y)
+        y = [2 if i == -1 else i for i in y]
+        y = [3 if i == -2 else i for i in y]
 
         return x, y
 
-    # todo 错别字处理，语义不明确词语处理，拼音繁体处理等
-    def correct_wrong_words(self, corpus):
-        return corpus
-
     # 去掉停用词
-    def clean_stop_words(self, sentences):
-        stop_words = None
-        with open("./stop_words.txt", "r") as f:
-            stop_words = f.readlines()
-            stop_words = [word.replace("\n", "") for word in stop_words]
+    # def clean_stop_words(self, sentences):
+    #     stop_words = None
+    #     with open("./stop_words.txt", "r") as f:
+    #         stop_words = f.readlines()
+    #         stop_words = [word.replace("\n", "") for word in stop_words]
+    #
+    #     # stop words 替换
+    #     for i, line in enumerate(sentences):
+    #
+    #         for word in stop_words:
+    #             if word in line:
+    #                 line = line.replace(word, "")
+    #         sentences[i] = line
+    #
+    #     return sentences
 
-        # stop words 替换
-        for i, line in enumerate(sentences):
+    def segmentWord(self,cont):
+        corpus = []
+        for i in cont:
+            a = list(jieba.cut(i))
+            b = " ".join(a).replace('，', '').replace('。', '').replace('？', '') \
+                .replace('！', '').replace('“', '').replace('”', '').replace('：', '') \
+                .replace('…', '').replace('（', '').replace('）', '').replace('—', '') \
+                .replace('《', '').replace('》', '').replace('、', '').replace('‘', '') \
+                .replace('’', '').replace('"', '').replace('~', '').replace('#', '').replace('【', '') \
+                .replace('】', '').replace('.', '').replace('^', '').replace('_', '').replace('(', '') \
+                .replace(')', '').replace('～', '').replace("'", '').replace(':', '')
 
-            for word in stop_words:
-                if word in line:
-                    line = line.replace(word, "")
-            sentences[i] = line
+            corpus.append(b)
 
-        return sentences
-
-    # 分词，将不在词向量中的jieba分词单独挑出来，他们不做分词
-    def get_words_after_jieba(self, sentences):
-        # jieba分词
-        all_exclude_words = dict()
-        while (1):
-            words_after_jieba = [[w for w in jieba.cut(line) if w.strip()] for line in sentences]
-            # 遍历不包含在word2vec中的word
-            new_exclude_words = []
-            for line in words_after_jieba:
-                for word in line:
-                    if word not in self.word2vec_model.wv.vocab and word not in all_exclude_words:
-                        all_exclude_words[word] = 1
-                        new_exclude_words.append(word)
-                    elif word not in self.word2vec_model.wv.vocab:
-                        all_exclude_words[word] += 1
-
-            # 剩余未包含词小于阈值，返回分词结果，结束。否则添加到jieba del_word中，然后重新分词
-            if len(new_exclude_words) < 10:
-                print("length of not in w2v words: %d, words are:" % len(new_exclude_words))
-                for word in new_exclude_words:
-                    print(word)
-                print("\nall exclude words are: ")
-                for word in all_exclude_words:
-                    if all_exclude_words[word] > 5:
-                        print("%s: %d," % (word, all_exclude_words[word]))
-                return words_after_jieba
-            else:
-                for word in new_exclude_words:
-                    jieba.del_word(word)
-
-        raise Exception("get_words_after_jieba error")
-
-    # 去除不在词向量中的词
-    def remove_words_not_in_embedding(self, corpus):
-        for i, sentence in enumerate(corpus):
-            for word in sentence:
-                if word not in self.word2vec_model.wv.vocab:
-                    sentence.remove(word)
-                    corpus[i] = sentence
-
+        # print(c)
         return corpus
 
     # 词向量，建立词语到词向量的映射
     def form_embedding(self, corpus):
-        # 1 读取词向量
-        w2v = dict(zip(self.word2vec_model.wv.index2word, self.word2vec_model.wv.syn0))
+        em_df = []
+        for line in corpus:
+            i = 0
+            line = line.split(' ')
+            for word in line:
+                try:
+                    line[i] = self.word2vec_model[word]
+                    i += 1
+                except:
+                    line[i] = np.zeros(150)
+                    i += 1
 
-        # 2 创建词语词典，从而知道文本中有多少词语
-        w2index = dict()        # 词语为key，索引为value的字典
-        index = 1
-        for sentence in corpus:
-            for word in sentence:
-                if word not in w2index:
-                    w2index[word] = index
-                    index += 1
-        print("\nlength of w2index is %d" % len(w2index))
+            if len(line) > 316:
+                line = line[:316]
 
-        # 3 建立词语到词向量的映射
-        # embeddings = np.random.randn(len(w2index) + 1, self.embedding_dim)
-        embeddings = np.zeros(shape=(len(w2index) + 1, self.embedding_dim), dtype=float)
-        embeddings[0] = 0   # 未映射到的词语，全部赋值为0
+            zero = np.zeros(150)
+            if len(line) < 316:
+                num_n = 316 - len(line)
+                for num in range(num_n):
+                    line = np.vstack((line,zero))
+            em_df.append(line)
+        em_df = np.array(em_df)
+        return em_df
 
-        n_not_in_w2v = 0
-        for word, index in w2index.items():
-            if word in self.word2vec_model.wv.vocab:
-                embeddings[index] = w2v[word]
-            else:
-                print("not in w2v: %s" % word)
-                n_not_in_w2v += 1
-        print("words not in w2v count: %d" % n_not_in_w2v)
 
-        del self.word2vec_model, w2v
-
-        # 4 语料从中文词映射为索引
-        x = [[w2index[word] for word in sentence] for sentence in corpus]
-
-        return embeddings, x
-
-    # 预处理，主函数
     def process(self):
         # 读取原始文件
         x, y = self.read_csv_file()
-
-        # 错别字，繁简体，拼音，语义不明确，等的处理
-        x = self.correct_wrong_words(x)
-
+        # print(type(x))
         # stop words
         # x = self.clean_stop_words(x)
-
         # 分词
-        x = self.get_words_after_jieba(x)
-
-        # remove不在词向量中的词
-        x = self.remove_words_not_in_embedding(x)
-
+        x = self.segmentWord(x)
+        # print(type(x))
         # 词向量到词语的映射
-        embeddings, x = self.form_embedding(x)
+        x = self.form_embedding(x)
+        y = np.array(y)
+        # print(x)
+        # print(type(x))
+        return  x, y
 
-        # 打印
-        print("embeddings[1] is, ", embeddings[1])
-        print("corpus after index mapping is, ", x[0])
-        print("length of each line of corpus is, ", [len(line) for line in x])
+class EncoderRNNWithVector(torch.nn.Module):
+    def __init__(self, hidden_size, out_size, n_layers=1, batch_size=1):
+        super(EncoderRNNWithVector, self).__init__()
 
-        return embeddings, x, y
+        self.batch_size = batch_size
+        self.hidden_size = hidden_size
+        self.n_layers = n_layers
+        self.out_size = out_size
+
+        # 这里指定了 BATCH FIRST
+        self.gru = torch.nn.GRU(hidden_size, hidden_size, n_layers, batch_first=True)
+
+        # 加了一个线性层，全连接
+        self.out = torch.nn.Linear(hidden_size, out_size)
+
+    def forward(self, word_inputs, hidden):
+        # -1 是在其他确定的情况下，PyTorch 能够自动推断出来，view 函数就是在数据不变的情况下重新整理数据维度
+        # batch, time_seq, input
+        inputs = word_inputs.view(self.batch_size, -1, self.hidden_size)
+
+        # hidden 就是上下文输出，output 就是 RNN 输出
+        output, hidden = self.gru(inputs, hidden)
+
+        output = self.out(output)
+
+        # 仅仅获取 time seq 维度中的最后一个向量
+        # the last of time_seq
+        output = output[:, -1, :]
+
+        return output, hidden
+
+    def init_hidden(self):
+
+        hidden = torch.autograd.Variable(torch.zeros(self.n_layers, 1, self.hidden_size))
+        return hidden
+
+
+def _test_rnn_rand_vec(x,y,x_valid,y_valid):
+    _xs = x
+    _ys = y
+
+    x_valid = x_valid
+    y_valid = y_valid
+    #
+    # x_test = x_test
+    # y_test = y_test
+
+    # 隐层 200，输出 6，隐层用词向量的宽度，输出用标签的值得个数 （one-hot)
+    model = EncoderRNNWithVector(200, 4)
+
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+    epoch_n = 10
+
+    train_labels = []
+    train_pred = []
+
+    for i in range(len(_xs)): # 以此遍历每句话
+        for epoch in range(epoch_n):
+            encoder_hidden = model.init_hidden()
+
+            input_data = torch.autograd.Variable(torch.Tensor(_xs[i]))
+            output_labels = torch.autograd.Variable(torch.LongTensor([_ys[i]]))
+
+            # print(output_labels)
+
+            encoder_outputs, encoder_hidden = model(input_data, encoder_hidden)
+
+            optimizer.zero_grad()
+
+            loss = criterion(encoder_outputs, output_labels)
+            loss.backward()
+            optimizer.step()
+        encoder_outputs = torch.argmax(encoder_outputs,dim=1)
+        encoder_outputs = encoder_outputs.data.numpy()[0]
+        output_labels = output_labels.data.numpy()[0]
+
+        train_labels.append(output_labels)
+        train_pred.append(encoder_outputs)
+        print(i, output_labels, encoder_outputs)
+        if i % 20 == 0:
+            print('--' * 20)
+
+    ff = f1_score(train_labels, train_pred, average='macro')       #
+    print('训练集已训练完,f1值是:{}'.format(ff))
+
+
+    valid_labels = []
+    valid_pred = []
+    for j in range(len(x_valid)):
+        encoder_hidden = model.init_hidden()
+
+        input_data2 = torch.autograd.Variable(torch.Tensor(x_valid[j]))
+        output_labels2 = torch.autograd.Variable(torch.LongTensor([y_valid[j]]))
+
+        encoder_outputs2,encoder_hidden2 = model(input_data2,encoder_hidden)
+
+        encoder_outputs2 = torch.argmax(encoder_outputs2, dim=1)
+
+        encoder_outputs2 = encoder_outputs2.data.numpy()[0]
+        output_labels2 = output_labels2.data.numpy()[0]
+
+        valid_labels.append(output_labels2)
+        valid_pred.append(encoder_outputs2)
+
+        print(j, output_labels2, encoder_outputs2)
+        if j % 20 == 0:
+            print('--' * 20)
+    print('--'*20)
+    ff = f1_score(valid_labels, valid_pred, average='macro')
+    print(ff)
+    return
+
+
 h = PreProcessor('./data/trainingset.csv')
-h.process()
+s,c = h.process()
+h1 = PreProcessor('./data/validationset.csv')
+s1,c1 = h1.process()
+x = _test_rnn_rand_vec(s,c,s1,c1)
